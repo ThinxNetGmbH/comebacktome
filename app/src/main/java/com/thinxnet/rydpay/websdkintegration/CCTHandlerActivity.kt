dfb2af7201d3c7410df3b6399bc975f4b4bd9b56
Lyubomir Ganev <lyubomir.ganev@thinxnet.com>
2021-09-10 11:39:27 +0200

    Update to gradle 7, implement handling for back from CCT and add rydpay default colors

--- app/src/main/java/com/thinxnet/rydpay/websdkintegration/CCTHandlerActivity.kt
+++ app/src/main/java/com/thinxnet/rydpay/websdkintegration/CCTHandlerActivity.kt
@@ -4,8 +4,17 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.browser.customtabs.CustomTabColorSchemeParams
 import androidx.browser.customtabs.CustomTabsIntent
+import androidx.browser.customtabs.CustomTabsIntent.*
+import androidx.core.app.BundleCompat
+import androidx.core.content.ContextCompat
+import androidx.core.os.bundleOf
+import androidx.core.os.postDelayed
+import com.thinxnet.rydpay.R
 import com.thinxnet.rydpay.websdkintegration.model.PaymentDataDto
 import com.thinxnet.rydpay.websdkintegration.model.PaymentDataDtoMapper
 import com.thinxnet.rydpay.websdkintegration.model.RydPayWebSdkCallbackResult
@@ -19,27 +28,79 @@ class CCTHandlerActivity : AppCompatActivity() {
         }
     }
 
+    private val autoFinishWithCancelRunnable = Runnable {
+        if (!isStartedWithCallbackIntent) {
+            finishWithCancel()
+        }
+    }
+
+    private val autoFinisHandler = Handler(Looper.getMainLooper())
+
+    private val isStartedWithInitialIntent: Boolean
+        get() = intent?.getParcelableExtra<Uri>(INTENT_EXTRA_URI) != null
+
+    private val isStartedWithCallbackIntent: Boolean
+        get() = intent?.data != null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val callbackUri = intent?.data
-        val initialUri = intent?.getParcelableExtra<Uri>(INTENT_EXTRA_URI)
-
         when {
-            callbackUri != null -> {
-                processCallbackUri(callbackUri)
+            isStartedWithCallbackIntent -> {
+                // Most probably activity was killed in the background, and is now created due to
+                // the custom url schema callback
+                val callBackUri = checkNotNull(intent?.data) {
+                    "Unexpected null callback intent data"
+                }
+                processCallbackUri(callBackUri)
             }
-            initialUri != null -> {
-                val builder = CustomTabsIntent.Builder()
-                val customTabsIntent = builder.build()
-                customTabsIntent.launchUrl(this, initialUri)
+            isStartedWithInitialIntent -> {
+                // Open the CCT only if this onCreate is the initial one and is not
+                // cause by a config change or the Activity being killed in the background
+                if (savedInstanceState == null) {
+                    val initialUri =
+                        checkNotNull(intent.getParcelableExtra<Uri>(INTENT_EXTRA_URI)) {
+                            "Unexpected null initial intent extra URI"
+                        }
+                    // Manually build and launch empty CCT intent,
+                    // since there is no support in the CCT public API for
+                    // startActivityForResult calls.
+                    val cctIntent = Intent(Intent.ACTION_VIEW).apply {
+                        putExtra(EXTRA_ENABLE_INSTANT_APPS, true)
+                        putExtra(EXTRA_SHARE_STATE, SHARE_STATE_DEFAULT)
+                        putExtra(
+                            EXTRA_TOOLBAR_COLOR,
+                            ContextCompat.getColor(this@CCTHandlerActivity, R.color.light_navy_blue)
+                        )
+                        val requiredSession = Bundle().apply {
+                            putBinder(EXTRA_SESSION, null)
+                        }
+                        putExtras(requiredSession)
+                        data = initialUri
+                    }
+                    startActivityForResult(cctIntent, CCT_REQUEST_CODE)
+                }
             }
             else -> finishWithCancel()
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CCT_REQUEST_CODE) {
+            // We just came back from the CCT. We should wait a bit to be sure that
+            // no onNewIntent call will happen before we can assume the user simply closed the CCT
+            autoFinisHandler.postDelayed(autoFinishWithCancelRunnable, 500)
+        } else {
+            finishWithCancel()
+        }
+    }
+
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
+        // We have received a new intent. If we have any pending auto-close,
+        // we should cancel it, since we want to provide back the successful result if possible
+        autoFinisHandler.removeCallbacks(autoFinishWithCancelRunnable)
         val callbackUri = intent?.data
         if (callbackUri == null) {
             finishWithCancel()
@@ -53,7 +114,8 @@ class CCTHandlerActivity : AppCompatActivity() {
         val paymentDataString = callbackUri.getQueryParameter("paymentdata")
         if (isPaymentSuccessful && paymentDataString != null) {
             try {
-                val paymentData = json.decodeFromString(PaymentDataDto.serializer(), paymentDataString)
+                val paymentData =
+                    json.decodeFromString(PaymentDataDto.serializer(), paymentDataString)
                 finishWithResult(
                     RydPayWebSdkCallbackResult.Success(PaymentDataDtoMapper.transform(paymentData))
                 )
@@ -76,7 +138,10 @@ class CCTHandlerActivity : AppCompatActivity() {
     }
 
     private fun finishWithResult(rydPayWebSdkCallbackResult: RydPayWebSdkCallbackResult) {
-        setResult(RESULT_OK, Intent().putExtra(INTENT_EXTRA_CALLBACK_RESULT, rydPayWebSdkCallbackResult))
+        setResult(
+            RESULT_OK,
+            Intent().putExtra(INTENT_EXTRA_CALLBACK_RESULT, rydPayWebSdkCallbackResult)
+        )
         finish()
     }
 
@@ -85,6 +150,7 @@ class CCTHandlerActivity : AppCompatActivity() {
         private const val INTENT_EXTRA_URI = "INTENT_EXTRA_URI"
 
         private const val INTENT_EXTRA_CALLBACK_RESULT = "INTENT_EXTRA_CALLBACK_RESULT"
+        private const val CCT_REQUEST_CODE = 42
 
         fun createLaunchIntent(context: Context, uri: Uri): Intent =
             Intent(context, CCTHandlerActivity::class.java).apply {
